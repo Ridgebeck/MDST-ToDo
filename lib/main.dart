750cc17a80b5ba9ca966c3dc8c9c9714a6c4c5df
Niklas <beck.niklas@gmail.com>
2021-02-01 00:07:00 -0800

    initialize firebase via async await in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/welcome_screen.dart';
-import 'screens/loading_screen.dart';
-import 'screens/error_screen.dart';
+//import 'screens/loading_screen.dart';
+//import 'screens/error_screen.dart';
 import 'package:flutter/services.dart';
 import 'util/task_data.dart';
 import 'util/timers.dart';
@@ -11,13 +11,14 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await sharedPrefs.init();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // Create the initialization Future outside of `build`:
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  //final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
@@ -26,63 +27,61 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
     ]);
 
-    return
-
-        //   MultiProvider(
-        //   providers: [
-        //     ChangeNotifierProvider(create: (_) {
-        //       TaskData taskData = TaskData();
-        //       // initialize download stream for community data
-        //       taskData.firebaseDataStream();
-        //       return taskData;
-        //     }),
-        //     ChangeNotifierProvider(create: (_) => MDSTTimer()),
-        //   ],
-        //   //child:
-        //   child: MaterialApp(
-        //     title: 'MDST_todo',
-        //     theme: ThemeData(canvasColor: Colors.transparent),
-        //     debugShowCheckedModeBanner: false,
-        //     home: WelcomeScreen(),
-        //   ),
-        //   //),
-        // );
-
-        FutureBuilder(
-      // Initialize FlutterFire
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return ErrorScreen();
-        }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MultiProvider(
-            providers: [
-              ChangeNotifierProvider(create: (_) {
-                TaskData taskData = TaskData();
-                // initialize download stream for community data
-                taskData.firebaseDataStream();
-                return taskData;
-              }),
-              ChangeNotifierProvider(create: (_) => MDSTTimer()),
-            ],
-            //child:
-            child: MaterialApp(
-              title: 'MDST_todo',
-              theme: ThemeData(canvasColor: Colors.transparent),
-              debugShowCheckedModeBanner: false,
-              home: WelcomeScreen(),
-            ),
-            //),
-          );
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        return LoadingScreen();
-      },
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) {
+          TaskData taskData = TaskData();
+          // initialize download stream for community data
+          taskData.firebaseDataStream();
+          return taskData;
+        }),
+        ChangeNotifierProvider(create: (_) => MDSTTimer()),
+      ],
+      //child:
+      child: MaterialApp(
+        title: 'MDST_todo',
+        theme: ThemeData(canvasColor: Colors.transparent),
+        debugShowCheckedModeBanner: false,
+        home: WelcomeScreen(),
+      ),
+      //),
     );
+
+    //     FutureBuilder(
+    //   // Initialize FlutterFire
+    //   future: _initialization,
+    //   builder: (context, snapshot) {
+    //     // Check for errors
+    //     if (snapshot.hasError) {
+    //       return ErrorScreen();
+    //     }
+    //
+    //     // Once complete, show your application
+    //     if (snapshot.connectionState == ConnectionState.done) {
+    //       return MultiProvider(
+    //         providers: [
+    //           ChangeNotifierProvider(create: (_) {
+    //             TaskData taskData = TaskData();
+    //             // initialize download stream for community data
+    //             taskData.firebaseDataStream();
+    //             return taskData;
+    //           }),
+    //           ChangeNotifierProvider(create: (_) => MDSTTimer()),
+    //         ],
+    //         //child:
+    //         child: MaterialApp(
+    //           title: 'MDST_todo',
+    //           theme: ThemeData(canvasColor: Colors.transparent),
+    //           debugShowCheckedModeBanner: false,
+    //           home: WelcomeScreen(),
+    //         ),
+    //         //),
+    //       );
+    //     }
+    //
+    //     // Otherwise, show something whilst waiting for initialization to complete
+    //     return LoadingScreen();
+    //   },
+    // );
   }
 }
